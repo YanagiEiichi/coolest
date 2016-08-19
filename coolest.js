61ea45d29e1857b7d41e560dda282e16976e7210
YanagiEiichi <admin@web-tinker.com>
2016-08-20 03:07:46 +0800

    Fix error catch

--- coolest.js
+++ coolest.js
@@ -2,24 +2,20 @@ module.exports = (generator, ...binds) => {
   let result = function(...args) {
     return new Promise((resolve, reject) => {
       let iterator = generator.call(this, ...args);
-      const errorHandler = error => {
-        try {
-          iterator.throw(error);
-        } catch (error) {
-          reject(error);
-        }
-      };
-      void function callee(value) {
+      void function callee(value, isError) {
         for (;;) {
           try {
-            let result = iterator.next(value);
+            let result = isError ? iterator.throw(value) : iterator.next(value);
+            isError = false;
             value = result.value;
             if (result.done) return resolve(value);
           } catch (error) {
-            return errorHandler(error);
+            return reject(error);
           }
           if (value && typeof value.then === 'function') {
-            return value.then(callee, errorHandler);
+            return value.then(callee, error => {
+              callee(error, true);
+            });
           }
         }
       }();
--- tests/catch.js
+++ tests/catch.js
@@ -2,21 +2,32 @@ const assert = require('assert');
 const coolest = require('../coolest');
 
 const f = coolest(function*() {
+  let p = 0;
   try {
     yield Promise.reject('ok');
+    throw 'hehe';
   } catch (result) {
     assert.equal(result, 'ok');
+    p++; 
+  }
+  try {
+    yield Promise.reject('ok');
+    throw 'hehe';
+  } catch (result) {
+    assert.equal(yield 1, 1);
+    p++; 
   }
   try {
     throw 'ok';
   } catch (result) {
     assert.equal(result, 'ok');
+    p++; 
   }
-  throw true;
+  throw p;
 });
 
 f().catch(result => {
-  assert(result);
+  assert.equal(result, 3);
   process.exit(0);
 });
 
